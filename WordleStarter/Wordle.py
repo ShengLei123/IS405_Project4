4abf6d3ab3a0378a74532572ba098845414c921d
ShengLei123 <starkray666666@gmail.com>
2022-10-03 13:33:59 -0600

    milestone 2

--- WordleStarter/Wordle.py
+++ WordleStarter/Wordle.py
@@ -13,21 +13,28 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 def wordle():
 
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        temp_string = ''
+        for x in range(0, N_COLS):
+            temp_string += gw.get_square_letter(row=0, col=x)
+        temp_string = temp_string.lower()
+        print(temp_string)
+        if temp_string in FIVE_LETTER_WORDS:
+            gw.show_message("Keep guessing")
+        else:
+            gw.show_message("No in word list")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
 # Startup code
-
     word = random.choice(FIVE_LETTER_WORDS).upper()
-    col_num = N_COLS - 1
-    row_num = N_ROWS - 1
     
-    col_num = 0
+    temp_col = 0
     for x in word:
-        gw.set_square_letter(row=0, col=col_num, ch=x)
-        col_num += 1
+        gw.set_square_letter(row=0, col=temp_col, ch=x)
+        temp_col += 1
+    
+    gw.add_enter_listener(enter_action)
 
 if __name__ == "__main__":
     wordle()
